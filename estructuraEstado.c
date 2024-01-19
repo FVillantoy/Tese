e5296cd1d49fdefba132cde552e5b7fc51257b15
fvillantoy <fvillantoy@unsa.edu.pe>
2024-01-19 11:04:59 -0500

    sa solucione los problemas de sintaxis, pero hay un problema de violacion del segmento xd

--- estructuraEstado.c
+++ estructuraEstado.c
@@ -1,20 +1,20 @@
 #include <stdlib.h>
-#include <stdios.h>
+#include <stdio.h>
 
 struct node{
     struct node* next;
     char value;
     struct state* key;
 
-}
+};
 struct state{
     struct node* transitions;
     char name;
     char acept_state; //es un booleano, para saber si es estado de aceptacion o no;
-}
+};
 void insertTran(struct node** transitions, char tranname, struct state* newstate){
-    struct node* current = transitions*;
-    struct node* newtransition = (node*)malloc(sizeof(node));
+    struct node* current = *transitions;
+    struct node* newtransition = (struct node*)malloc(sizeof(struct node));
     newtransition->next = NULL;
     newtransition->value = tranname;
     newtransition->key = newstate;
@@ -27,19 +27,21 @@ void insertTran(struct node** transitions, char tranname, struct state* newstate
 void insertState(struct state** stateq, char tranname, char is_acept){//se pasa el estado donde le vamos a agregar y lo que pasa con la transicion
     struct state* newstate = (struct state*)malloc(sizeof(struct state));
     struct node* newtransition = (struct node*)malloc(sizeof(struct node));
-    
     newtransition->next = NULL;
     newtransition->value = 0;
-    newtransition->state = NULL;
-
+    newtransition->key = NULL;
+    
     newstate->transitions = newtransition;
     newstate->name = 0; //aun no se si le seguire poniendo nombre
     newstate->acept_state = is_acept;
-
-    insertTran(&((stateq*)->transitions), tranname, newstate);
+    if(*stateq){
+        *stateq = newstate;
+        return;
+    }
+    insertTran(&((*stateq)->transitions), tranname, newstate);
 }
 void PrintS(struct state * pstate){
-    printf("%c",pstate->value);
+    printf("%c",pstate->name);
     struct node* current = pstate->transitions;
     while(current){
         printf("%c", current->value);
@@ -49,6 +51,8 @@ void PrintS(struct state * pstate){
 }
 
 int main(){
-    struct state* estado; //TODO 
-    
+    struct state* estado = NULL; //TODO 
+    insertState(&estado, 'a', 0);
+    PrintS(estado);
+    return 0;
 }
